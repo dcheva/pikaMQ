23547410ba57c0e8c93a71a4340dab6a0f566e4d
Dmytro Cheusov <d.cheusov@abona-erp.com>
2020-02-04 08:36:04 +0200

    Use try consuming

--- receive.py
+++ receive.py
@@ -2,18 +2,22 @@
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+    host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
+
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % (body,))
 
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
 
-channel.start_consuming()
+channel.basic_consume('hello', callback, False)
+
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+connection.close()
